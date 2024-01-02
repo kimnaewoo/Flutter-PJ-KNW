35ae6f1a21c43528bbb051098f70ca279179bded
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2024-01-02 11:21:16 +0900

    App_03_profile setup

---
+++ 03.플러터PJ/app_03_pofile/android/app/src/main/kotlin/com/example/app_03_pofile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_03_pofile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 03.플러터PJ/app_03_pofile/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key : key);
+
+  // 앱 빌드 위젯 재정의 !
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // 디버그 모드 자이구
+      debugShowCheckedModeBanner: false,
+      // 테마를 위한 클래스를 만들고 불러오기 
+      
+    );
+  }
+}
+
