8a0025479f515a6859e5bd0dcb32026ee340343b
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2024-01-08 09:26:06 +0900

    ss

---
+++ 03.플러터PJ/app_03_profile/lib/components/profile_buttons.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class ProfileButtons extends StatelessWidget {
+  const ProfileButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        _buildFollowButton(),
+        _buildMessageButton(),
+      ],
+    );
+  }
+
+  Widget _buildFollowButton() {
+    return InkWell(
+      onTap: () {
+        print("Follow 버튼 클릭됨");
+      },
+      child: Container(
+        alignment: Alignment.center,
+        width: 150,
+        height: 45,
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Text(
+          "Follow",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMessageButton() {
+    return InkWell(
+      onTap: () {
+        if (kDebugMode) {
+          print("Message 버튼 클릭됨");
+        }
+      },
+      child: Container(
+        alignment: Alignment.center,
+        width: 150,
+        height: 45,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(),
+        ),
+        child: Text(
+          "Message",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
